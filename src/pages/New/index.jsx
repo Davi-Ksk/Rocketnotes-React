b2526c5ca7741064278010bdfde9e8da5a173612
davi <davikraieski@gmail.com>
2023-06-05 00:07:52 -0300

    tags now can be added and deleted, also links and tags can't be added if they are blank

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Header } from '../../components/Header';
 import { Input } from '../../components/Input';
 import { TextArea } from '../../components/TextArea';
@@ -8,7 +9,46 @@ import { Link } from "react-router-dom";
 
 import { Container, Form } from "./style";
 
+
+
 export function New() {
+
+    const [links, setLinks ] = useState([]); //guarda todos os links
+    const [newLink, setNewLink] = useState(""); //link adicionado no momento
+
+    const [tags, setTags ] = useState([]); 
+    const [newTag, setNewTag] = useState(""); 
+
+    // handles
+    //--------------------------------------------------------------------
+
+    function handleAddLink() {
+        if(!newLink) {
+            alert("Ops! Digite algo no seu link!");
+        } else {
+            setLinks(prevState => [...prevState, newLink]); //checa o que tinha antes, monta tudo que tinha antes com o novo link
+            setNewLink("");
+        }
+    }
+
+    function handleRemoveLink(deleted) {
+        setLinks(prevState => prevState.filter(link => link !== deleted));
+    }
+
+    function handleAddTag() {
+        if(!newTag) {
+            alert("Ops! Digite algo na sua tag!");
+        } else {
+            setTags(prevState => [...prevState, newTag]); //checa o que tinha antes, monta tudo que tinha antes com o novo link
+            setNewTag("");
+        }
+    }
+
+    function handleRemoveTag(deleted) {
+        setTags(prevState => prevState.filter(tag => tag !== deleted));
+    }
+
+
     return(
         <Container>
             <Header/>
@@ -25,14 +65,43 @@ export function New() {
                     <TextArea placeholder="Observações"/>
 
                     <Section title="Links úteis">
-                        <NoteItem value="https://github.com/rocketseat"/>
-                        <NoteItem isNew placeholder="Novo link" />
+                        {
+                            links.map((link, index) => (
+                                <NoteItem 
+                                key={String(index)}//sempre colocado em componentes listados
+                                value={link}
+                                onClick={() => handleRemoveLink(link)} //quando houver um parametro para função, usa-se arrow function
+                            />
+                            ))                  
+                        }
+                        <NoteItem 
+                            isNew 
+                            placeholder="Novo link"
+                            value={newLink}
+                            onChange={(e) => setNewLink(e.target.value)}
+                            onClick={handleAddLink}
+                        />
                     </Section>
 
-                    <Section title="Marcadores">
+                    <Section title="Marcadores">                        
                         <div className="tags">
-                            <NoteItem value="react" />
-                            <NoteItem isNew placeholder="Nova Tag" />
+                            {
+                                tags.map((tag, index) => (
+                                    <NoteItem
+                                    key={String(index)} 
+                                    value={tag} 
+                                    onClick={() => handleRemoveTag(tag)}
+                                />
+                                ))
+                            }
+                            {/* <NoteItem value="react" /> */}
+                            <NoteItem 
+                                isNew
+                                placeholder="Nova Tag"
+                                value={newTag}
+                                onChange={(e) => setNewTag(e.target.value)}
+                                onClick={handleAddTag}
+                            />
                         </div>
                     </Section>
 
